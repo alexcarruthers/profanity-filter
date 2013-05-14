e91f7f7dc19cd93913d63996f6cfcccb85128c38
Alex Carruthers <alex@alexcarruthers.com>
2013-05-14 12:53:35 -0400

    Added ability to check string for swear words and updated readme

--- README.md
+++ README.md
@@ -1,12 +1,14 @@
 profanity-filter
 ================
 
-Python module that replaces inappropriate words with something more PG rated.    
+Python module that replaces inappropriate words with something more PG rated.
+Can also check if a string contains swear words 
 
 Usage
 -----   
 ```python
-f = Filter('badword and bad words', clean_word='unicorn')
-safe_string = f.clean()
+f = Filter(clean_word='unicorn')
+safe_string = f.clean('badword and bad words')
 print safe_string
+f.check('badword and notbadword')
 ```
--- profanity_filter.py
+++ profanity_filter.py
@@ -2,29 +2,37 @@
 Uses a line separated file listing bad words as it's source
 to check if a user submitted something inappropriate
 
-f = Filter('Annoyingwhores and fudge packers', clean_word='unicorn')
-word = f.clean()
+f = Filter(clean_word='unicorn')
+word = f.clean('Annoyingwhores and fudge packers')
 print word
 >>Annoyingunicorns and unicorns
+f.check('whore')
+>>True
 """
 import re
 
 class Filter(object):
     """
     Replaces a bad word in a string with something more PG friendly
-    
-    Filter('you annoying prick', 'unicorn')
-    
+
+    Also checks to see if a string contains words that are profane
+        
     """
-    def __init__(self, original_string, clean_word='****'):
+    def __init__(self, clean_word='****'):
         
         bad_words_file = open('bad_words.txt', 'r')
         
-        self.bad_words = set(line.strip('\n') for line in open('bad_words.txt'))
-        self.original_string = original_string
+        self.bad_words = set(line.strip('\n') + '\\b' for line in open('bad_words.txt'))
         self.clean_word = clean_word
         
-    def clean(self):
+    def clean(self, string):
         exp = '(%s)' %'|'.join(self.bad_words)
         r = re.compile(exp, re.IGNORECASE)
-        return r.sub(self.clean_word, self.original_string)
+        return r.sub(self.clean_word, string)
+
+    def check(self, string):
+        exp = '(%s)' %'|'.join(self.bad_words)
+        if re.match(exp, string.lower()):
+            return True
+        else:
+            return False
